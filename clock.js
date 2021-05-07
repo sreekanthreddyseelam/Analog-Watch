135c1de03011b08048e371d6b1e55a23e98d1df1
SreeKanthReddy Seelam <sreekanthreddyseelam@outlook.com>
2021-05-07 16:55:18 +0530

    first commit

---
+++ clock.js
@@ -0,0 +1,16 @@
+var secondHand = document.querySelector("#sec");
+var minHand = document.querySelector("#min");
+var hourHand = document.querySelector("#hr");
+
+setInterval(clockRotating, 1000);
+
+function clockRotating() {
+  var date = new Date();
+  var getSeconds = date.getSeconds() / 60;
+  var getMinutes = date.getMinutes() / 60;
+  var getHours = date.getHours() / 12;
+
+  secondHand.style.transform = "rotate(" + getSeconds * 360 + "deg)";
+  minHand.style.transform = "rotate(" + getMinutes * 360 + "deg)";
+  hourHand.style.transform = "rotate(" + getHours * 360 + "deg)";
+}
